09486738da264b1a0000fe5d08a8c7e4b394c3e2
Cristian Castillo <cristian.castillo@salud.gob.sv>
2025-08-21 12:58:48 -0600

    product enpoint completo

--- src/main/java/com/heladeria/heladeria/controller/ProductController.java
+++ src/main/java/com/heladeria/heladeria/controller/ProductController.java
@@ -3,12 +3,11 @@ package com.heladeria.heladeria.controller;
 import com.heladeria.heladeria.model.Product;
 import com.heladeria.heladeria.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("helados")
@@ -21,4 +20,26 @@ public class ProductController {
     public List<Product> getProducts(){
         return productService.getProduct();
     }
+
+    @PostMapping("/product")
+    public Product saveProduct(@RequestBody Product product){
+        return productService.saveProduct(product);
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProductByID(@PathVariable Long id) {
+        Product product = productService.findProductById(id);
+        return ResponseEntity.ok(product);
+    }
+
+    @DeleteMapping("product/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id){
+        boolean eliminado = productService.deleteProduct(id);
+        if (eliminado) {
+            return ResponseEntity.noContent().build(); // 204 si se borró
+        } else {
+            return ResponseEntity.notFound().build(); // 404 si no existe
+        }
+    }
+
 }
