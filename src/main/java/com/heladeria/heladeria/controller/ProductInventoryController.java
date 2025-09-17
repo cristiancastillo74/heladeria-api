1c6e509aba98203e12fd4504f4f6a40f65cb9ff1
Cristian Castillo <cristian74425@gmail.com>
2025-09-16 21:49:06 -0600

    listar el inventario de product

---
+++ src/main/java/com/heladeria/heladeria/controller/ProductInventoryController.java
@@ -0,0 +1,27 @@
+package com.heladeria.heladeria.controller;
+
+
+
+import com.heladeria.heladeria.model.ProductInventory;
+import com.heladeria.heladeria.service.ProductInventoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("helados")
+@CrossOrigin(value = "http://localhost:55555")
+public class ProductInventoryController {
+
+    @Autowired
+    private ProductInventoryService pis;
+
+    @GetMapping("/productInventory")
+    public List<ProductInventory> getPI(){
+        return pis.getProductProductInventory();
+    }
+}
