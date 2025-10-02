616304754effe84109fa347066e40281b7be4623
CRIS <cristian.castillo@salud.gob.sv>
2025-10-02 15:11:36 -0600

    crud about Product's Inventory

--- src/main/java/com/heladeria/heladeria/controller/ProductInventoryController.java
+++ src/main/java/com/heladeria/heladeria/controller/ProductInventoryController.java
@@ -5,10 +5,8 @@ package com.heladeria.heladeria.controller;
 import com.heladeria.heladeria.model.ProductInventory;
 import com.heladeria.heladeria.service.ProductInventoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +21,26 @@ public class ProductInventoryController {
     @GetMapping("/productInventory")
     public List<ProductInventory> getPI(){
         return pis.getProductProductInventory();
+    }
+
+    @GetMapping("/productInventory/{id}")
+    public ResponseEntity<ProductInventory> getProInventById(@PathVariable Long id){
+        ProductInventory pid = pis.findProductInventoryById(id);
+        return ResponseEntity.ok(pid);
+    }
+
+    @PostMapping("/productInventory")
+    public ProductInventory save(@RequestBody ProductInventory productInventory){
+        return pis.saveProductInventory(productInventory);
+    }
+
+    @DeleteMapping("/productInventory/{id}")
+    public ResponseEntity<Void> deleteProInvent(@PathVariable Long id){
+        boolean eliminado = pis.deleteProductInventory(id);
+        if (eliminado){
+            return ResponseEntity.noContent().build();
+        }else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
