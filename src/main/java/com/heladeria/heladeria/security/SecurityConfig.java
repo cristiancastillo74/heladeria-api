41d9e5d9de1d1dfd3cb77efb227b0d463ed1368f
critiancastillo74 <cristian74425@gmail.com>
2025-11-19 16:54:18 -0600

    arreglando botones de crear y editar de inv. cylinder

--- src/main/java/com/heladeria/heladeria/security/SecurityConfig.java
+++ src/main/java/com/heladeria/heladeria/security/SecurityConfig.java
@@ -96,7 +96,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://rival:5055")); // o "*" temporalmente
+        configuration.setAllowedOrigins(List.of("http://localhost:5055", "http://rival:5055")); // o "*" temporalmente
         configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
